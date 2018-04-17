58803dfb33e2dff6fa3a7365aa56978f887c29d9
xuchunxiao <522784020@qq.com>
2018-04-17 11:17:12 +0800

    Add create module node script add jslib

---
+++ khaos/README.md
@@ -0,0 +1,4 @@
+# Khaos 
+Create Module Expamle:
+node khaos.js moduleName pageName stateName
+Please replace the name with the define of yours
--- khaos/khaos.js
+++ khaos/khaos.js
@@ -1,73 +1,60 @@
 const fs = require('fs');
+const copydir = require('copy-dir');
+const rimraf = require('rimraf');
 
-let basepath = 'src/components/';
-const moment = require('moment');
+copydir.sync('./template', './temp');
 
-const cptName = process.argv.splice(2)[0];
-const path = cptName.split('/');
-const name = path[path.length - 1];
-const writes = [`${name}.js`, `${name}.html`, `${name}.less`, 'index.js'];
-const reads = [`${basepath}cptTemp/index.js`, `${basepath}cptTemp/cptTemp.js`];
-const file = [];
-const author = require('os').homedir().split('\\').pop();
+const ModuleName = process.argv[2];
+const PageName = process.argv[3];
+const stateName = process.argv[4];
 
 
-// 检测是否存在文件夹
-const exists = function () {
-  return new Promise((res, rej) => {
-    (async function () {
-      for (const a of path) {
-        fs.existsSync(basepath + a) ? basepath = `${basepath}${a}/` : await mkdir(a);
-      }
-      res(basepath);
-    }());
-  });
-};
-// 建立文件夹
-let mkdir = function (a) {
-  return new Promise((res, rej) => {
-    fs.mkdir(basepath + a, (err) => {
-      if (err) rej(err);
-      basepath = `${basepath}${a}/`;
-      res(basepath);
-    });
-  });
-};
-// 读取模板文件内容，并替换为目标组件
-const readFile = function () {
-  return new Promise((res) => {
-    for (const a of reads) {
-      let text = fs.readFileSync(a).toString();
-      text = text.replace(/time/g, moment().format('YYYY/MM/DD'))
-        .replace(/temp/g, name)
-        .replace(/author/g, author);
-      file.push(text);
+function geFileList(path) {
+  const filesList = [];
+  readFile(path, filesList);
+  return filesList;
+}
+
+function readFile(path, filesList) {
+  const files = fs.readdirSync(path);
+  files.forEach(walk);
+  function walk(file) {
+    states = fs.statSync(`${path}/${file}`);
+    if (states.isDirectory()) {
+      const newpath = `${path}/${file.replace('ModuleName', ModuleName).replace('PageName', PageName)}`;
+      fs.renameSync(`${path}/${file}`, newpath, (err) => {
+        if (err) console.log(`ERROR: ${err}`);
+      });
+      readFile(newpath, filesList);
+    } else {
+      const obj = new Object();
+      obj.size = states.size;
+      obj.name = file;
+      const newPath = `${path}/${file.replace('ModuleName', ModuleName).replace('PageName', PageName)}`;
+      obj.path = newPath;
+
+      fs.renameSync(`${path}/${file}`, newPath, (err) => {
+        if (err) console.log(`ERROR: ${err}`);
+      });
+      fs.readFile(newPath, 'utf8', (err, data) => {
+        if (err) {
+          return console.log(err);
+        }
+        const result = data.replace(/ModuleName/g, ModuleName).replace(/PageName/g, PageName).replace(/stateName/g, stateName);
+
+        fs.writeFile(newPath, result, 'utf8', (err1) => {
+          if (err1) return console.log(err);
+        });
+      });
+      filesList.push(obj);
     }
-    res(file);
-  });
-};
-// 生成文件，并填入之前读取的文件内容
-const writeFile = function (file) {
-  return new Promise((res, rej) => {
-    (async function () {
-      for (const a of writes) {
-        await fs.writeFile(`${basepath}${a}`,
-          a == writes[3] ? file[0] : a == writes[0] ? file[1] : '', (err) => {
-            if (err) rej(err);
-          });
-      }
-      res('succ');
-    }());
-  });
-};
-async function creatCpt() {
-  try {
-    await exists();
-    await readFile();
-    await writeFile(await readFile());
-    return console.log(`Successfully created ${name} component`);
-  } catch (err) {
-    console.error(err);
   }
 }
-creatCpt();
+
+geFileList('./temp');
+
+setTimeout(() => {
+  copydir.sync(`./temp/${ModuleName}`, `../src/pages/${ModuleName}`);
+  rimraf('./temp', () => {});
+  console.log(`Khaos create module ${ModuleName} successfully`);
+}, 3000);
